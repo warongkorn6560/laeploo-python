fafe4bb35ec8647db0e1829ead6949853c2c9670
Warongkorn Tritipakit <warongkorn5lab@Warongkorns-MacBook-Air.local>
2024-03-23 16:03:26 +0700

    :alien: Update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask, request
-from Thaispoon import Spoonerism
+from laeplooth import loo
 
 app = Flask(__name__)
 
@@ -9,14 +9,14 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     """Example Hello World route."""
-    return "R U LOST!!!"
+    return "หลอกดูก!"
 
 # Create a GET /translate that accepts keyword arguments for the text and returns the spoonerism
 @app.route("/translate")
 def translate():
     """Return the spoonerism of the given text."""
     text = request.args.get("text", "")
-    return Spoonerism(text).Loolang()
+    return loo(text)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
